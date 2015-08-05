64b4b1b14d8d466992a1a4ac79f35414525a8961
alishalisha <alisha.ramos@voxmedia.com>
2015-08-05 11:24:42 -0400

    adding new structure and clearCanvas function

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 context = document.getElementById('canvas').getContext("2d");
 
+// -------------------------------
+//
+// set up basic canvas dimensions
+//
+// -------------------------------
+
+var canvasWidth = context.canvas.width;
+var canvasHeight = context.canvas.height;
+var containerWidth = document.getElementById("canvas-container").offsetWidth;
+var containerHeight = document.getElementById("canvas-container").offsetHeight;
+
+// this will make the canvas element responsive
+canvasWidth = containerWidth;
+canvasHeight = containerHeight;
+
 var image = new Image();
 image.src = "https://cdn0.vox-cdn.com/thumbor/l-aJK1tYUa6XazJiOStAgLOInEk=/0x0:2222x1667/1200x900/filters:format(webp)/cdn0.vox-cdn.com/uploads/chorus_image/image/46868926/DrielyS-5168.0.0.0.0.jpg";
 
@@ -7,6 +22,12 @@ image.onload = function() {
   imageWidth = this.naturalWidth;
   imageHeight = this.naturalHeight;
 }
+
+// -------------------------------
+//
+// set up basic drawing functions
+//
+// -------------------------------
 
 $('#canvas').mousedown(function(e){
   var mouseX = e.pageX - this.offsetLeft;
@@ -39,14 +60,6 @@ var clickX = new Array();
 var clickY = new Array();
 var clickDrag = new Array();
 var paint;
-var canvasWidth = context.canvas.width;
-var canvasHeight = context.canvas.height;
-var containerWidth = document.getElementById("canvas-container").offsetWidth;
-var containerHeight = document.getElementById("canvas-container").offsetHeight;
-
-// this will make the canvas element responsive
-canvasWidth = containerWidth;
-canvasHeight = containerHeight;
 
 function addClick(x, y, dragging)
 {
@@ -57,32 +70,6 @@ function addClick(x, y, dragging)
 
 function redraw(){
   context.clearRect(0, 0, canvasWidth, canvasHeight); // Clears the canvas
-  
-  var sx = (imageWidth * .25);
-  var sy = (imageHeight * .25);
-  var sWidth = (sx * 2);
-  var sHeight = (sy * 2);
-  var aspectRatio = (imageWidth/imageHeight);
-  
-  // if the image width is greater than the height, the image width should be the destination width
-  
-  if (imageWidth > imageHeight) {
-    var dy = (canvasHeight - (canvasWidth/aspectRatio))/2;
-    var dx = 0;
-    context.drawImage(image, sx, sy, sWidth, sHeight, dx, dy, canvasWidth, (canvasHeight/aspectRatio));
-    // take the remainder of 800/aspect ratio, divide it in half, and set the sy to that.
-  } else {
-    var dx = (canvasWidth - (canvasHeight*aspectRatio))/2;
-    var dy = 0;
-    context.drawImage(image, sx, sy, sWidth, sHeight, dx, dy, (canvasHeight*aspectRatio), canvasHeight);
-  }
-  
-    document.getElementById("left-crop").onclick = function() {
-      var sx = 0;
-      var sy = 0;
-      context.drawImage(image, sx, sy, sWidth, sHeight, dx, dy, (canvasHeight*aspectRatio), canvasHeight);
-    }
-  
   context.strokeStyle = "yellow";
   context.lineJoin = "round";
   context.lineWidth = 5;
@@ -99,3 +86,29 @@ function redraw(){
      context.stroke();
   }
 }
+
+// -------------------------------
+//
+// tools functions
+//
+// -------------------------------
+
+function clearCanvas(){
+  context.clearRect(0, 0, canvasWidth, canvasHeight);
+
+  // empty the points arrays
+  clickX = [];
+  clickY = [];
+  clickDrag = [];
+
+  console.log("clearing");
+}
+
+// -------------------------------
+//
+// call tools functions
+//
+// -------------------------------
+
+var clearButton = document.getElementById("canvas-clear");
+clearButton.onclick = function() { clearCanvas(); }
