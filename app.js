3b11af01e0ea8fef92098cb3f642d5b9d97070f1
alishalisha <alisha.ramos@voxmedia.com>
2015-08-06 14:00:13 -0400

    notes for self, image editing, and empty more arrays

--- app.js
+++ app.js
@@ -39,6 +39,20 @@ $('#canvas').mousedown(function(e){
   redraw();
 });
 
+/* the drag and resize function may be in conflict with
+the 'mousedown' and dragging function we have for drawing.
+therefore....
+we need to have two separate things happening:
+1) user is ONLY SHOWN the "drop an image here" or "upload an image"
+prompt
+2) then they can drag and resize/scale the image to how they want.
+3) then they can set the status of the image to "done" if they're happy with it.
+4) only if the status of the image is "done" can the other canvas mousedown function up there be
+called. then the drawing controls will be shown.
+5) if the status of the image is switched back to "editing" then the canvas mousedown function for drawing
+will be disabled again and the drawing tools will be hidden again (or faded out?). :|
+*/
+
 $('#canvas').mousemove(function(e){
   var mouseX = e.pageX - this.offsetLeft;
   var mouseY = e.pageY - this.offsetTop;
@@ -68,6 +82,14 @@ var curColor = "purple";
 var paint;
 var colorTools = document.getElementsByClassName("controls__colors-color");
 var clearButton = document.getElementById("canvas-clear");
+var imageStatus = image.dataset.status
+imageStatus = 'editing';
+
+if (imageStatus == 'editing') {
+  console.log("you are editing");
+} else {
+  console.log("image is done.");
+}
 
 function addClick(x, y, dragging)
 {
@@ -113,12 +135,12 @@ function redraw(){
 function clearCanvas(){
   context.clearRect(0, 0, canvasWidth, canvasHeight);
 
-  // empty the points arrays
+  // empty the points arrays and other arrays
   clickX = [];
   clickY = [];
   clickDrag = [];
-
-  console.log("clearing");
+  clickColor = [];
+  clickSize = [];
 }
 
 function resizeImage(){
